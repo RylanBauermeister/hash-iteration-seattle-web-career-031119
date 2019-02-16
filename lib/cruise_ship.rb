49b41b5d7801502efa1e63a74558139b6019e1f7
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-16 02:04:30 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,9 +10,8 @@ def select_winner(passengers)
   # add the code snippet here!
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
-      winner = name
+      return name
       break
     end
   end
-  winner
 end
